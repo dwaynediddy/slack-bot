2bba89a138c6d41f6f545e414b5ccf11face265c
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-19 15:38:01 +0200

    clean up code

--- bot.py
+++ bot.py
@@ -7,13 +7,12 @@ load_dotenv()
 
 # Access the 'SLACK_BOT_TOKEN' environment variable
 slack_bot_token = os.getenv('SLACK_BOT_TOKEN')
-#absolute joke that this has issues and dotenv has issues 
+
 env_path = Path('.') / '.env'
 
 slack_bot_token = os.environ['SLACK_BOT_TOKEN']
 client = WebClient(token=slack_bot_token)
 
-# client = WebClient(token='xoxb-5926145488753-5916507028626-2VFvmUEeymx1jx3tSMaB0s2r')
 
 
 client.chat_postMessage(channel='#test', text='hello world!')
