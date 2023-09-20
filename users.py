4b7899eb6bd78e8502f3ed215b9fc80de77377e4
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-20 11:25:19 +0200

    bot sent DM but cant reply

--- users.py
+++ users.py
@@ -9,16 +9,21 @@ slack_bot_token = os.environ['SLACK_BOT_TOKEN']
 client = WebClient(token=slack_bot_token)
 
 # Specify the user ID as the channel parameter
-user_id = 'U05TJES4796'
+# user_id = 'U05TJES4796'
+user_id='U05T6MR0CBW'
 
-# Message content
-message_text = 'Hello, this is a private message to the user.'
+response = client.conversations_open(users=[user_id])
 
-# Send a private message to the user
-client.chat_postMessage(
-    channel=user_id,
-    text=message_text
-)
+if response['ok']:
+    conversation_id = response['channel']['id']
+    message = 'This is a DM from a Bot'
+    
+    client.chat_postMessage(
+        channel=conversation_id,
+        text=message
+    )
+else:
+    print('failed to DM')
 
 
 
