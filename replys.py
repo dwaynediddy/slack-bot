8d208975422321981dc978796db2e0a6f089aa3e
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-21 12:12:14 +0200

    slack api dms have unique ids this now retrieves conversations history

---
+++ replys.py
@@ -0,0 +1,35 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the WebClient with your bot's token
+slack_bot_token = os.environ['SLACK_BOT_TOKEN']
+client = WebClient(token=slack_bot_token)
+
+conversation_id = 'D05SVH4BXDK'  # conversation ID can be DM or channel
+
+try:
+    # Use the conversations.history method to retrieve messages
+    response = client.conversations_history(
+        channel=conversation_id,
+        limit=100  # more than 200 is not recoomended
+    )
+
+    if response['ok']:
+        messages = response['messages']
+        for message in messages:
+            user_id = message['user']
+            message_text = message['text']
+
+            # Process and use the user's reply as needed
+            print(f"User ID: {user_id}")
+            print(f"Message Text: {message_text}")
+    else:
+        print('Failed to retrieve messages. Error:', response['error'])
+
+except SlackApiError as e:
+    print(f"Error: {e.response['error']}")
+    
