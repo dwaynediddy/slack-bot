f6698c5b98416f2e110ecb78b2eed2a6767932f2
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-26 12:08:36 +0200

    sending message on schedule

--- server.py
+++ server.py
@@ -19,6 +19,8 @@ conversation_id = 'D05SVH4BXDK'
 user_id = 'U05TJES4796'
 # user_id = 'U05T6MR0CBW'  # test acc
 
+test_channel_id = 'C05SVHU936Z' #test channel id
+
 
 # create table if it doesn't exist
 cursor.execute("CREATE TABLE IF NOT EXISTS my_slack_bot (message TEXT, sender TEXT)")
@@ -70,8 +72,29 @@ def store_dm(message, sender):
     cursor.execute("INSERT INTO my_slack_bot (message, sender) VALUES (?, ?)", (message, sender))
     conn.commit()
     print(f"Stored DM: {message} from {sender}")
+    
+# Function to send a message to the test channel
+def send_scheduled_messaage(message_text):
+    try:
+        client.chat_postMessage(
+            channel='#test',  # Replace with your test channel name or ID
+            text=message_text
+        )
+        print(f'Sent message: {message_text} to #test channel')
+    except SlackApiError as e:
+        print(f"Error sending message to #test channel: {e.response['error']}")
+
+# Schedule a message to be sent to the test channel at a specific datetime
+def schedule_message():
+    scheduled_time = datetime.datetime(2023, 9, 26, 12, 8)  # Adjust the datetime as needed
+    current_time = datetime.datetime.now()
+    
+    if current_time >= scheduled_time:
+        message_text = "This is a scheduled message to #test channel"
+        send_scheduled_messaage(message_text)
 
 # Periodically check for new messages and store them
 while True:
     get_and_store_new_messages()
-    time.sleep(10)  # interval in seconds
+    schedule_message()
+    time.sleep(6000)  # interval in seconds
