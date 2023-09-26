11d0eb25cbb818081e8c25d6712d814ecd2b47ba
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-26 11:37:21 +0200

    now showing display name instead of user_id

--- server.py
+++ server.py
@@ -5,21 +5,25 @@ from dotenv import load_dotenv
 import sqlite3
 import datetime
 import time
+import logging
 
 load_dotenv()
 
 slack_bot_token = os.environ['SLACK_BOT_TOKEN']
 client = WebClient(token=slack_bot_token)
 
+logger = logging.getLogger(__name__)
+
 # connect to sqlite
 conn = sqlite3.connect('my_slack_bot.db')
 cursor = conn.cursor()
 
 conversation_id = 'D05SVH4BXDK'
-user_id = 'U05TJES4796' #diddy
+user_id = 'U05TJES4796'
+# user_id = 'U05T6MR0CBW'  # test acc
 
 
-# create table if it doesnt exist
+# create table if it doesn't exist
 cursor.execute("CREATE TABLE IF NOT EXISTS my_slack_bot (message TEXT, sender TEXT)")
 
 cursor.execute("INSERT INTO my_slack_bot VALUES ('hello world', 'diddy')")
@@ -33,16 +37,34 @@ def get_and_store_new_messages():
     try:
         response = client.conversations_history(
             channel=conversation_id,
-            limit=1 
+            limit=1
         )
 
         if response['ok']:
             messages = response['messages']
             for message in messages:
                 if 'user' in message and 'text' in message:
-                    sender = message['user']
+                    sender_id = message['user']
                     message_text = message['text']
-                    store_dm(message_text, sender)
+
+                    # Fetch user information to get the user's display name
+                    try:
+                        user_info_response = client.users_info(user=sender_id)
+                        if user_info_response['ok']:
+                            user_info = user_info_response['user']
+                            if 'profile' in user_info and 'display_name' in user_info['profile']:
+                                sender_name = user_info['profile']['display_name']
+                            else:
+                                sender_name = user_info['real_name_normalized'] if 'real_name_normalized' in user_info else sender_id
+                                print('User display name not found, using real name')
+                        else:
+                            sender_name = sender_id
+                            print('User info request failed')
+                    except SlackApiError as e:
+                        print(f"Error fetching user info: {e.response['error']}")
+                        sender_name = sender_id
+
+                    store_dm(message_text, sender_name)
 
     except SlackApiError as e:
         print(f"Error: {e.response['error']}")
