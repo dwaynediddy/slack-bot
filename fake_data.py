bfa5db4ebe231e5db8a77b7c4008a33dfa591df8
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-28 11:12:52 +0200

    printing to see fake data

--- fake_data.py
+++ fake_data.py
@@ -19,7 +19,15 @@ for _ in range(10): # number of fake dms
     sender = random.choice(fake_users)
     message = f"Fake message from {sender}: {random.choice(['Hello', 'Hi', 'How are you?', 'Testing'])}"
     insert_fake_dm(message, sender)
+
+# Retrieve and print the fake DMs
+cursor.execute("SELECT message, sender FROM my_slack_bot")
+fake_dms = cursor.fetchall()
+
+for dm in fake_dms:
+    message, sender = dm
+    print(f"Sender: {sender}\nMessage: {message}\n")
     
-    conn.close()
+conn.close()
 
     
