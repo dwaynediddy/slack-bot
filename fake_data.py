3d3487b79d20f773f72bf058fbca6632136e142c
dwaynediddy <burrows.dwayne@icloud.com>
2023-09-28 14:20:28 +0200

    deployment push

--- fake_data.py
+++ fake_data.py
@@ -17,7 +17,7 @@ def insert_fake_dm(message, sender):
 fake_users = ['randy', 'super hans', 'james', 'bob', 'ted']
 for _ in range(10): # number of fake dms
     sender = random.choice(fake_users)
-    message = f"Fake message from {sender}: {random.choice(['Hello World', 'I am Happy today', 'Are you well??', 'Testing'])}"
+    message = f"Fake message from {sender}: {random.choice(['testing again', 'this is some new messages', 'i wanna be in that girls club?', 'bing bong','lots more fake messages', '100 years rick and morty','nintendo give me free stuff'])}"
     insert_fake_dm(message, sender)
 
 # Retrieve and print the fake DMs
--- server.py
+++ server.py
@@ -87,16 +87,10 @@ def schedule_message():
     current_time = datetime.datetime.now()
     # set this line to the when you want the bot to post using for testing over a period of time
     if (
-<<<<<<< HEAD
-        (current_time.weekday() == 2 and current_time.hour == 21 and current_time.minute == 30) or
-        (current_time.weekday() == 2 and current_time.hour == 23 and current_time.minute == 45) or
-        (current_time.weekday() == 3 and current_time.hour == 1 and current_time.minute == 22) or
-        (current_time.weekday() == 3 and current_time.hour == 3 and current_time.minute == 22) 
-=======
-        (current_time.weekday() == 3 and current_time.hour == 11 and current_time.minute == 19) or
-        (current_time.weekday() == 3 and current_time.hour == 12 and current_time.minute == 19) or
-        (current_time.weekday() == 3 and current_time.hour == 1 and current_time.minute == 22)
->>>>>>> testing
+        (current_time.weekday() == 3 and current_time.hour == 11 and current_time.minute == 59) or
+        (current_time.weekday() == 3 and current_time.hour == 12 and current_time.minute == 45) or
+        (current_time.weekday() == 3 and current_time.hour == 13 and current_time.minute == 22) or
+        (current_time.weekday() == 3 and current_time.hour == 14 and current_time.minute == 22) 
     ):
         send_latest_unsent_dms()
         
