6ab7ec7867a4ecaf8ec14af0415cf7b95a99f72d
dwaynediddy <burrows.dwayne@icloud.com>
2023-10-13 10:24:43 +0200

    saving attempt at url request with slack events

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from slack_sdk import WebClient
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
+slack_events_adapter = SlackEventAdapter(os.getenv('SLACK_SIGNING_SECRET'), "/slack/events")
 
 slack_bot_token = os.getenv('SLACK_BOT_TOKEN')
 client = WebClient(token=slack_bot_token)
 
-@app.route('/slack/events', methods=['GET'])
+@app.route('/slack/events', methods=['POST'])
 def slack_event_callback():
-    return 'hello world'
+    if slack_events_adapter.server.verify_signature(request):
+        return 'Verified'
+    else:
+        return 'Verification failed', 403
+
+@slack_events_adapter.on("app_mention")
+def handle_message(event_data):
+    # Handle app mentions
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
